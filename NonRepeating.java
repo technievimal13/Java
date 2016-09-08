13bc49cd8a520b9a2bb0878c08784daeb9bb0a10
Vimalraj <technievimal@gmail.com>
2016-09-08 14:56:57 +0530

    Create NonRepeating.java

---
+++ NonRepeating.java
@@ -0,0 +1,13 @@
+public class NonRepeating {
+    public static void main(String[] args) {
+        int a[] = { 3, 4, 1, 3, 1, 7, 2, 2, 4 };
+        System.out.println(nonRepeatingElement(a));
+    }
+    private static int nonRepeatingElement(int[] a) {
+        int element = 0;
+        for (int i = 0; i < a.length; i++) {
+            element ^= a[i];
+            System.out.println(element);
+        }
+        return element;
+    }
